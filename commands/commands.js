b37dc8b709b6bd42d16c9294100db4e6c07d006d
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2022-03-06 09:28:13 -0600

    CommandRunner class.

--- commands/commands.js
+++ commands/commands.js
@@ -1,92 +1,101 @@
 "use strict";
 
-const axiosInstance = axios.create({
-    timeout: 1000,
-    headers: {
-      'axios-version': axios.VERSION
-    }
-  });
+class CommandRunner {
 
-  const fetchInfoForCommandID = async (commandID) => {
-    try {
-      const response = await axiosInstance.get(`/cgi-bin/commands/${commandID}`);
-
-      handleFetchInfoForCommandIDResponse(response.data);
-    } catch (error) {
-      console.error('fetch error:', error);
-    }
-  }
-
-  const handleFetchInfoForCommandIDResponse = (responseData) => {
-    const outputPre = document.querySelector('#output');
-
-    let commandAndArgsString = responseData.commandInfo.command;
-    for (const arg of (responseData.commandInfo.args || [])) {
-      commandAndArgsString += ` ${arg}`;
+    constructor() {
+        this.axiosInstance = axios.create({
+            timeout: 1000,
+            headers: {
+                'axios-version': axios.VERSION
+            }
+        });
+        this.fetchAllCommandsTryNumber = 0;
     }
 
-    let preText = `Now: ${responseData.now}\n\n`;
-    preText += `Command Duration: ${responseData.commandDuration}\n\n`;
-    preText += `$ ${commandAndArgsString}\n\n`;
-    preText += responseData.commandOutput;
+    async fetchInfoForCommandID(commandID) {
+        try {
+            const response = await axiosInstance.get(`/cgi-bin/commands/${commandID}`);
 
-    outputPre.innerText = preText;
-  }
+            handleFetchInfoForCommandIDResponse(response.data);
+        } catch (error) {
+            console.error('fetch error:', error);
+        }
+    }
 
-  let fetchAllCommandsTryNumber = 0;
+    handleFetchInfoForCommandIDResponse(responseData) {
+        const outputPre = document.querySelector('#output');
 
-  const fetchAllCommands = async () => {
-    try {
-      ++fetchAllCommandsTryNumber;
+        let commandAndArgsString = responseData.commandInfo.command;
+        for (const arg of (responseData.commandInfo.args || [])) {
+            commandAndArgsString += ` ${arg}`;
+        }
 
-      const response = await axiosInstance.get('/cgi-bin/commands');
+        let preText = `Now: ${responseData.now}\n\n`;
+        preText += `Command Duration: ${responseData.commandDuration}\n\n`;
+        preText += `$ ${commandAndArgsString}\n\n`;
+        preText += responseData.commandOutput;
 
-      handleFetchAllCommandsResponse(response.data);
-    } catch (error) {
-      console.error('fetch error:', error);
+        outputPre.innerText = preText;
+    }
 
-      const commandsDiv = document.querySelector("#commands");
-      commandsDiv.innerHTML = `<pre>Try number ${fetchAllCommandsTryNumber} to fetch all commands failed.</pre>`;
+    async fetchAllCommands() {
+        try {
+            ++fetchAllCommandsTryNumber;
 
-      setTimeout(() => {
+            const response = await axiosInstance.get('/cgi-bin/commands');
+
+            handleFetchAllCommandsResponse(response.data);
+        } catch (error) {
+            console.error('fetch error:', error);
+
+            const commandsDiv = document.querySelector("#commands");
+            commandsDiv.innerHTML = `<pre>Try number ${fetchAllCommandsTryNumber} to fetch all commands failed.</pre>`;
+
+            setTimeout(() => {
+                fetchAllCommands();
+            }, 1000);
+        }
+    }
+
+    handleFetchAllCommandsResponse(commands) {
+        // generate the radio buttons
+        const commandsDiv = document.querySelector("#commands");
+
+        commandsDiv.innerHTML = (commands || []).map((command) =>
+            `<div>
+               <input type="radio" name="command" value="${command.id}" id="${command.id}">
+               <label for="${command.id}">${command.description}</label>
+             </div>`)
+            .join('\n');
+
+        const radioButtons = document.querySelectorAll('input[name="command"]');
+
+        for (const radioButton of radioButtons) {
+            radioButton.addEventListener('change', function (e) {
+                if (this.checked) {
+                    const selectedCommandID = radioButton.value;
+                    fetchInfoForCommandID(selectedCommandID);
+                }
+            });
+        }
+
+        setInterval(() => {
+            for (const radioButton of radioButtons) {
+                if (radioButton.checked) {
+                    const selectedCommandID = radioButton.value;
+                    fetchInfoForCommandID(selectedCommandID);
+                    break;
+                }
+            }
+        }, 1000);
+    }
+
+    start() {
         fetchAllCommands();
-      }, 1000);
-    }
-  }
-
-  const handleFetchAllCommandsResponse = (commands) => {
-    // generate the radio buttons
-    const commandsDiv = document.querySelector("#commands");
-
-    commandsDiv.innerHTML = (commands || []).map((command) => 
-       `<div>
-          <input type="radio" name="command" value="${command.id}" id="${command.id}">
-          <label for="${command.id}">${command.description}</label>
-        </div>`)
-      .join(' ');
-
-    const radioButtons = document.querySelectorAll('input[name="command"]');
-
-    for (const radioButton of radioButtons) {
-      radioButton.addEventListener('change', function (e) {
-        if (this.checked) {
-          const selectedCommandID = radioButton.value;
-          fetchInfoForCommandID(selectedCommandID);
-        }
-      });
     }
 
-    setInterval(() => {
-      for (const radioButton of radioButtons) {
-        if (radioButton.checked) {
-          const selectedCommandID = radioButton.value;
-          fetchInfoForCommandID(selectedCommandID);
-          break;
-        }
-      }
-    }, 1000);
-  }
+}
 
-  const onload = () => {
-    fetchAllCommands();
-  }
+const onload = () => {
+    new CommandRunner().start();
+}

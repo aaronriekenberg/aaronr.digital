c7945ff75bea9b3df44da8231ccc226299654746
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2022-11-05 08:26:39 -0500

    Add reobots.txt

--- scripts/brotli-static.sh
+++ scripts/brotli-static.sh
@@ -2,4 +2,4 @@
 
 find . -name \*.br | xargs rm -f
 
-find . -name \*.html -o -name \*.js -o -name \*.map -o -name \*.css -o -name \*.ico | xargs brotli -k
+find . -name \*.html -o -name \*.js -o -name \*.map -o -name \*.css -o -name \*.ico -o -name \*.txt | xargs brotli -k

ce369c0c9ce9e110299501b8d6897302d51eeb73
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2022-12-29 12:19:22 -0600

    Use rust-parallel.

--- scripts/brotli-static.sh
+++ scripts/brotli-static.sh
@@ -1,5 +1,7 @@
-#!/bin/sh
+#!/bin/sh -x
 
 find . -name \*.br | xargs rm -f
 
-find . -name \*.html -o -name \*.js -o -name \*.map -o -name \*.css -o -name \*.ico -o -name \*.txt | xargs brotli -k
+#find . -name \*.html -o -name \*.js -o -name \*.map -o -name \*.css -o -name \*.ico -o -name \*.txt | xargs brotli -k
+
+find . -name \*.html -o -name \*.js -o -name \*.map -o -name \*.css -o -name \*.ico -o -name \*.txt | awk '{printf "brotli -k %s\n", $1}' | rust-parallel

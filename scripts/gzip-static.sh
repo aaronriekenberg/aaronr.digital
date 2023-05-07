141a98925055cbba5a4d4297752f95fb8f162828
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2023-05-07 16:21:42 -0500

    Update to sync with brotli-static.sh.

--- scripts/gzip-static.sh
+++ scripts/gzip-static.sh
@@ -1,5 +1,7 @@
 #!/bin/sh -x
 
-find . -name \*.gz | xargs rm -f
+find . -name \*.gz -print0 | xargs -0 rm -f
 
-find . -name \*.html -o -name \*.js -o -name \*.map -o -name \*.css -o -name \*.ico | xargs gzip -k
+#find . -name \*.html -o -name \*.js -o -name \*.map -o -name \*.css -o -name \*.ico -o -name \*.txt | xargs gzip -k
+
+find . \( -name \*.html -o -name \*.js -o -name \*.map -o -name \*.css -o -name \*.ico -o -name \*.txt \) -print0 | rust-parallel -0 gzip -k

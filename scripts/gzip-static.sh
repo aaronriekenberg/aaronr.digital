a09be1816c60a034fbfd46ec630c795c30f39946
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2024-01-23 17:02:58 -0600

    Debian favicon.

--- scripts/gzip-static.sh
+++ scripts/gzip-static.sh
@@ -4,4 +4,4 @@ set -e
 
 find . -name \*.gz -print0 | rust-parallel -0 rm -f
 
-find . \( -name \*.html -o -name \*.js -o -name \*.map -o -name \*.css -o -name \*.txt \) -print0 | rust-parallel -0 gzip -k
+find . \( -name \*.html -o -name \*.js -o -name \*.map -o -name \*.css -o -name \*.txt -o name \*.ico \) -print0 | rust-parallel -0 gzip -k

36fb14d9b887d1e5d492dc58f6bad51e0132a1d0
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2025-02-08 05:03:23 -0600

    Add zstd, cleanup unused files.

--- scripts/brotli-static.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh -x
-
-find . -name \*.br -print0 | xargs -0 rm -f
-
-find . \( -name \*.html -o -name \*.js -o -name \*.map -o -name \*.css -o -name \*.ico -o -name \*.txt \) -print0 | rust-parallel -0 brotli -k
--- scripts/gzip-static.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -x
-
-set -e
-
-find . -name \*.gz -print0 | rust-parallel -0 rm -f
-
-find . \( -name \*.html -o -name \*.js -o -name \*.map -o -name \*.css -o -name \*.txt \) -print0 | rust-parallel -0 gzip -k

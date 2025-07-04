a4676dfc45349cccf0ec0d55e5fb9a5637d1d919
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2025-07-04 14:52:00 -0500

    Add --best to brotli.

--- scripts/compress.sh
+++ scripts/compress.sh
@@ -8,6 +8,6 @@ find . -name \*.gz -print0 | xargs -0 rm -f
 
 #find . \( -name \*.html -o -name \*.js -o -name \*.map -o -name \*.css -o -name \*.ico -o -name \*.txt \) -print0 | rust-parallel -0 zstd -k
 
-find . \( -name \*.html -o -name \*.js -o -name \*.map -o -name \*.css -o -name \*.ico -o -name \*.txt \) -print0 | rust-parallel -0 brotli -k
+find . \( -name \*.html -o -name \*.js -o -name \*.map -o -name \*.css -o -name \*.ico -o -name \*.txt \) -print0 | rust-parallel -0 brotli -k --best
 
 find . \( -name \*.html -o -name \*.js -o -name \*.map -o -name \*.css -o -name \*.ico -o -name \*.txt \) -print0 | rust-parallel -0 gzip -k

0ba421f22560c7d0109ec881c577e00917e4d854
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2025-11-23 12:27:43 -0600

    Revert "use xargs."
    This reverts commit 914cbf923486c0e54f522c10536e1de8943275f8.

--- scripts/compress.sh
+++ scripts/compress.sh
@@ -6,8 +6,8 @@ find . -name \*.br -print0 | xargs -0 rm -f
 
 find . -name \*.gz -print0 | xargs -0 rm -f
 
-find . \( -name \*.html -o -name \*.js -o -name \*.map -o -name \*.css -o -name \*.ico -o -name \*.txt \) -print0 | xargs -0 zstd -k
+find . \( -name \*.html -o -name \*.js -o -name \*.map -o -name \*.css -o -name \*.ico -o -name \*.txt \) -print0 | rust-parallel -0 zstd -k
 
-find . \( -name \*.html -o -name \*.js -o -name \*.map -o -name \*.css -o -name \*.ico -o -name \*.txt \) -print0 | xargs -0 brotli -k
+find . \( -name \*.html -o -name \*.js -o -name \*.map -o -name \*.css -o -name \*.ico -o -name \*.txt \) -print0 | rust-parallel -0 brotli -k
 
-find . \( -name \*.html -o -name \*.js -o -name \*.map -o -name \*.css -o -name \*.ico -o -name \*.txt \) -print0 | xargs -0 gzip -k
+find . \( -name \*.html -o -name \*.js -o -name \*.map -o -name \*.css -o -name \*.ico -o -name \*.txt \) -print0 | rust-parallel -0 gzip -k

3edb61bf4e7c01e8599b23a9750bb14bb8a0bfbf
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2026-02-23 12:04:54 -0600

    Revert "comment zstd and br"
    This reverts commit f0be335ed2dcbbbb7f314c1121d85380cbeb643f.

--- scripts/compress.sh
+++ scripts/compress.sh
@@ -1,13 +1,13 @@
 #!/bin/sh -x
 
-#find . -name \*.zst -print0 | xargs -0 rm -f
+find . -name \*.zst -print0 | xargs -0 rm -f
 
-#find . -name \*.br -print0 | xargs -0 rm -f
+find . -name \*.br -print0 | xargs -0 rm -f
 
 find . -name \*.gz -print0 | xargs -0 rm -f
 
-#find . \( -name \*.html -o -name \*.js -o -name \*.map -o -name \*.css -o -name \*.ico -o -name \*.txt \) -print0 | rust-parallel -0 zstd -k
+find . \( -name \*.html -o -name \*.js -o -name \*.map -o -name \*.css -o -name \*.ico -o -name \*.txt \) -print0 | rust-parallel -0 zstd -k
 
-#find . \( -name \*.html -o -name \*.js -o -name \*.map -o -name \*.css -o -name \*.ico -o -name \*.txt \) -print0 | rust-parallel -0 brotli -k
+find . \( -name \*.html -o -name \*.js -o -name \*.map -o -name \*.css -o -name \*.ico -o -name \*.txt \) -print0 | rust-parallel -0 brotli -k
 
 find . \( -name \*.html -o -name \*.js -o -name \*.map -o -name \*.css -o -name \*.ico -o -name \*.txt \) -print0 | rust-parallel -0 gzip -k
